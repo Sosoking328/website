b72987e48dfde78e4cecdc4a17860dc5921a9754
sosoking328 <king@sosokanapp.com>
2017-07-18 00:44:44 -0400

    add fav button

--- vote/models.py
+++ vote/models.py
@@ -6,6 +6,7 @@ class Album(models.Model):
     genre = models.CharField(max_length=100)
     album_logo = models.CharField(max_length=1000)
 
+
     def __str__(self):
         return self.artist + ' - ' + self.album_title
 
